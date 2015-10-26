7370e5d490da5b0e0b05d0132d72d4c68d199cb7
Maria Alejandra Montenegro <mariale@mariales-mbp.attlocal.net>
2015-10-26 10:57:25 -0700

    update main page with images link

--- Web/js/mainController.js
+++ Web/js/mainController.js
@@ -2,9 +2,9 @@
 function pageController(){
 	this.isActive = false;
 	
-	function setPage(project){ 
+	function setSinglePage(project){ 
  		$("#project_title").text (project.title);
-		$("#project_info").text (project.content);
+		$("#project_info").html (project.content);
 		$("#img1").attr("src",project.image1);
 		$("#img2").attr("src", project.image2);
 		$("#img3").attr("src", project.image3);
@@ -16,10 +16,104 @@ function pageController(){
 		else { $("#img4").attr("src", project.image4);}
 	}
 
-	function class2(){
-		 $("#text_1").text ("<p>chao</p>");
-	}
+	// loading more that one story in same pg
+	function setMultiPage(project){ 
 
+	 	var div = document.createElement('div');
+    	div.className = 'col s12 m12';
+    	div.innerHTML = '<div class="center-align">\
+        					<div class="project_info">\
+     							<h3 id="project_title_1"> title</h3>\
+     							<p id = "project_info_1" > main info dynamicly loaded </p>\
+     						</div>\
+     						<iframe id = "video_content_1" class="z-depth-2" width="560" height="315" src="" frameborder="0" allowfullscreen></iframe>\
+       					 	<br/><br/><br/><br/><br/>\
+       				 	</div>';
+    	document.getElementById('mainBody').appendChild(div);
+ 		$("#project_title_1").text (project.title);
+	 	$("#project_info_1").html (project.content);
+		$("#video_content_1").attr("src", project.video);
+	 	
+	 	var div2 = document.createElement('div');
+    	div2.className = 'col s12 m12';
+    	div2.innerHTML = '<div class="center-align">\
+        					<div class="project_info">\
+     							<h3 id="project_title_2"> title</h3>\
+     							<p id = "project_info_2" > main info dynamicly loaded </p>\
+     						</div>\
+     						<iframe id = "video_content_2" class="z-depth-2" width="560" height="315" src="" frameborder="0" allowfullscreen></iframe>\
+       					 	<br/><br/><br/><br/><br/>\
+       				 	</div>';
+    	document.getElementById('mainBody').appendChild(div2);
+		$("#project_title_2").text (project.title_2);
+		$("#project_info_2").html (project.content_2);
+		$("#video_content_2").attr("src", project.video_2);
+
+		div = document.createElement('div');
+    	div.className = 'col s12 m12';
+    	div.innerHTML = '<div class="center-align">\
+        					<div class="project_info">\
+     							<h3 id="project_title_3"> title</h3>\
+     							<p id = "project_info_3" > main info dynamicly loaded </p>\
+     						</div>\
+     						<iframe id = "video_content_3" class="z-depth-2" width="560" height="315" src="" frameborder="0" allowfullscreen></iframe>\
+       					 	<br/><br/><br/><br/><br/>\
+       				 	</div>';
+    	document.getElementById('mainBody').appendChild(div);
+ 		$("#project_title_3").text (project.title_3);
+	 	$("#project_info_3").html (project.content_3);
+		$("#video_content_3").attr("src", project.video_3);
+	 	
+	 	div2 = document.createElement('div');
+    	div2.className = 'col s12 m12';
+    	div2.innerHTML = '<div class="center-align">\
+        					<div class="project_info">\
+     							<h3 id="project_title_4"> title</h3>\
+     							<p id = "project_info_4" > main info dynamicly loaded </p>\
+     						</div>\
+     						<iframe id = "video_content_4" class="z-depth-2" width="560" height="315" src="" frameborder="0" allowfullscreen></iframe>\
+       					 	<br/><br/><br/><br/><br/>\
+       				 	</div>';
+    	document.getElementById('mainBody').appendChild(div2);
+		$("#project_title_4").text (project.title_4);
+		$("#project_info_4").html (project.content_4);
+		$("#video_content_4").attr("src", project.video_4);
+		
+		
+	}
+	function setDoublePage(project){ 
+
+	 	var div = document.createElement('div');
+    	div.className = 'col s12 m12';
+    	div.innerHTML = '<div class="center-align">\
+        					<div class="project_info">\
+     							<h3 id="project_title_1"> title</h3>\
+     							<p id = "project_info_1" > main info dynamicly loaded </p>\
+     						</div>\
+     						<iframe id = "video_content_1" class="z-depth-2" width="560" height="315" src="" frameborder="0" allowfullscreen></iframe>\
+       					 	<br/><br/><br/><br/><br/>\
+       				 	</div>';
+    	document.getElementById('mainBody').appendChild(div);
+ 		$("#project_title_1").text (project.title);
+	 	$("#project_info_1").html (project.content);
+		$("#video_content_1").attr("src", project.video);
+	 	
+	 	var div2 = document.createElement('div');
+    	div2.className = 'col s12 m12';
+    	div2.innerHTML = '<div class="center-align">\
+        					<div class="project_info">\
+     							<h3 id="project_title_2"> title</h3>\
+     							<p id = "project_info_2" > main info dynamicly loaded </p>\
+     						</div>\
+     						<iframe id = "video_content_2" class="z-depth-2" width="560" height="315" src="" frameborder="0" allowfullscreen></iframe>\
+       					 	<br/><br/><br/><br/><br/>\
+       				 	</div>';
+    	document.getElementById('mainBody').appendChild(div2);
+		$("#project_title_2").text (project.title_2);
+		$("#project_info_2").html (project.content_2);
+		$("#video_content_2").attr("src", project.video_2);
+		
+	}
 	this.service = function(param){
 		var project;
 		switch(param.call) {
@@ -38,11 +132,28 @@ function pageController(){
 		     case "kinect_fun":
 		    	project = new kinect_fun();
 		    	break;
+		    case "last_egg":
+		    	project = new last_egg();
+		    	break;
+		    case "shortGames":
+		    	project = new shortGames();
+		    	setDoublePage(project);
+		    	return;
+		    case "kinects":
+		    	project = new kinects();
+		    	setMultiPage(project);
+		    	return;
+		    case "linguar":
+		    	project = new linguar();
+		    	break;
+		    case "floors":
+		    	project = new floors();
+		    	break;
 		    default:
 		        break;
 		}
 
-		setPage(project);
+		setSinglePage(project);
 
 	}
 }
--- Web/js/projects.js
+++ Web/js/projects.js
@@ -19,8 +19,6 @@ function palmistry_ball(){
 	this.mainContent += "and record the 3D point of Leap motion and the 2D point of the screen where the mouse cursor was at. ";
 	this.mainContent += " <br/><br/>This infromation was then feeded into openCV to find the final position "
 	this.mainContent += "of the virtual camera in the scene and translate it there.";
-
-
 	this.video = "http://player.vimeo.com/video/127358563";
 }
 
@@ -28,35 +26,53 @@ function marioneta(){
 	this.title = "Marioneta";
 	this.content = "A puppet gesture recognition and mirroing experience done using Microsoft Kinect v2. ";
 	this.content += "It was a project for the Pittsburgh Children’s Museum and it is currently a permanently installatin.";
-	
 	this.mainContent = "Marioneta's main focus is to create an endless experience in which everything in the virtual world will ";
 	this.mainContent += "react to the users actions as they become and impersonate a puppet. "
 	this.mainContent += "It was addmited into SXSW 2015 Film Festival. ";
 	this.mainContent += "<br/><br/>";
 	this.mainContent += "I was the technical lead in this amazing project, and you can learn more about it by visiting out website:";
-
 	this.image1 = "images/marioneta/img1.png";
 	this.image2 = "images/marioneta/img2.png";
 	this.image3 = "images/marioneta/img3.png";
 	this.image4 = "images/marioneta/img4.png";
-
 	this.video = "https://www.youtube.com/embed/DKYC4Ey70Rk";
 }
 
 function molecap(){
 	this.title = "MoleCap VR";
-
-	this.content = "A research project involving wireless VR with the use of a motion capture system."
-	this.content += "The main purpose was to use the body as the main control for the experience."; 
-	this.content += "How do we react in a VR world when there are no cables or walls to act as boundaries? ";
-	this.content += "How do naive users react to day to day activities in a VR world? Done with the Oculus Rift in Unity 3D";
-
+	this.content = "A new virtual reality experience utilizing Wireless Oculus Rift DK1 "
+	this.content += "and the Organic Motion Capture System. "
+	this.mainContent = "MoleCap was a reaserch project at Carnegie Mellon University. Its main purpose is ";
+	this.mainContent += "to use the human body as the main control for the experience."; 
+	this.mainContent += "<br/><br/>";
+	this.mainContent += "How do we react in a VR world when there are no cables or walls to act as boundaries? ";
+	this.mainContent += "How do naive users react to day to day activities in a VR world?";
+	this.mainContent += "<br/><br/>";
+	this.mainContent += "I was one of the programmers and main game designer in this team."
 	this.image1 = "images/molecap/img1.png";
 	this.image2 = "images/molecap/img2.png";
 	this.image3 = "images/molecap/img3.png";
-	this.image4 = "images/molecap/img4.png";
-
+	this.image4 = "none";
 	this.video = "https://www.youtube.com/embed/PK4NhH9FANs";
+}
+
+function last_egg(){
+	this.title = "The Last Egg";
+
+	this.content = "A digital playground game for two teams of 3 players that uses the PlayStation Move Motion Controllers.<br/> "
+	this.content += "It was nominated at IndieCade 2014 as one of the 'Big Games' finalist."
+	this.mainContent = "The game consists of 3 rounds of 1 minute each. In which round both teams have to compete ";
+	this.mainContent += "to get the 'last egg'. When the game starts, the 'last egg' randomly selects one ";
+	this.mainContent += "PS Move and assigns a white light to it. This will indicate who was the egg."; 
+	this.mainContent += "<br/><br/>";
+	this.mainContent += "I was one of the programmers in this team."
+
+	this.image1 = "images/lastEgg/img1.jpg";
+	this.image2 = "images/lastEgg/img2.jpg";
+	this.image3 = "images/lastEgg/img3.jpg";
+	this.image4 = "images/lastEgg/img4.png";
+
+	this.video = "http://player.vimeo.com/video/98692480";
 }
 
 function maze_of_heart(){
@@ -73,21 +89,94 @@ function maze_of_heart(){
 	this.mainContent += "to its heart. The player will get help from a mouse, a friend of the robot, by indicating ";
 	this.mainContent += "the player in what part of the body the pieces are. This way the player will have to move ";
 	this.mainContent += "its body in multiple ways to effectively take the pieces back to the heart."
+	this.mainContent += "<br/><br/>";
+	this.mainContent += "I was one of the programmers in this team."
+	this.mainContent += "<br/><br/>";
+	this.mainContent += "To know more visit <a href = 'http://www.mazeofheart.fusion-sky.com' target = '_blank'>Maze of Heart </a>."
 
 	this.image1 = "images/mazeHeart/img1.png";
 	this.image2 = "images/mazeHeart/img2.png";
 	this.image3 = "images/mazeHeart/img3.png";
 	this.image4 = "images/mazeHeart/img4.png";
-
 	this.video = "http://player.vimeo.com/video/78123680";
 }
 
-function kinect_fun() {
-	this.title = "Kinect Stuff";
-	this.content = "hello";
+function shortGames(){
+	this.title = "Muzilla";
+	this.title_2 = "Against all Odds";
+	this.content = "An interactive audio/visual DJ experience that makes use of Leap Motion and ";
+	this.content += "Fast Fourier Transform for audio recognition."
 
-	this.image1 = "images/kinect/img1.png";
-	this.image3 = "images/kinect/img2.png";
-	this.image2 = "images/kinect/img3.png";
-	this.image4 = "images/molecap/img4.png";
+	this.content_2 = "An Oculus Rift experience where you get to ride a sperm and race to the egg. ";
+	this.content_2 += "At the egg you get to fight the other sperms to be the first one to fertilize the egg!";
+	this.video = "https://www.youtube.com/embed/x2sMIurf2sY";
+	this.video_2 = "http://player.vimeo.com/video/79898511";
 }
+
+function kinects() {
+	this.title = "Point Cloud Me";
+	this.content = "Inspired by the video of “point cloud dancer”.";
+	this.content += "I decided to make a sound reactive point cloud to give the user some control on what would ";
+	this.content += "happen throughout the simulation.<br/>A low pitch sound, will decompose the user and ";
+	this.content += "a high pitch sound will re-generate the user.<br/>";
+	this.content += "(FFT for audio recognition)"
+	this.video = "http://player.vimeo.com/video/50759765";
+
+	this.title_2 = "Noise Ball";
+	this.content_2 = "Noise Ball is a kinect simulation made in openFrameworks. Balls are created with different sound levels. ";
+	this.content_2 += "The main point of this simulation is to create noise to be able to interact with the balls. ";
+	this.video_2 = "http://player.vimeo.com/video/50020490";
+
+	this.title_3 = "Multiple Me";
+	this.content_3 = "Multiple Me was inspired from the typical video editing technical of having “multiple you’s” ";
+	this.content_3 += "on the screen. I decided to make it real time with the use of the kinect! it can hold up to 20 ";
+	this.content_3 += "different video clips. I really want to go to space, so that’s why it’s multiple me in space! ";
+	this.video_3 = "http://player.vimeo.com/video/50760404";
+
+	this.title_4 = "OpenGL Car";
+	this.content_4 = " My first Xbox kinect simulation. Made with ofxKinect drivers in OpenFrameworks. ";
+	this.content_4 += "The scene is made up of openGL primitives and you get to drive around a boxy blue car. ";
+	this.content_4 += "Physics is fully implemented so if you hit a wall to fast, you may flip!";
+	this.video_4 = "http://player.vimeo.com/video/49884803";
+	
+}
+
+function linguar() {
+this.title = "Linguar";
+
+	this.content = "A digital playground game for two teams of 3 players that uses the PlayStation Move Motion Controllers.<br/> "
+	this.content += "It was nominated at IndieCade 2014 as one of the 'Big Games' finalist."
+	this.mainContent = "The game consists of 3 rounds of 1 minute each. In which round both teams have to compete ";
+	this.mainContent += "to get the 'last egg'. When the game starts, the 'last egg' randomly selects one ";
+	this.mainContent += "PS Move and assigns a white light to it. This will indicate who was the egg."; 
+	this.mainContent += "<br/><br/>";
+	this.mainContent += "I was one of the programmers in this team."
+
+	this.image1 = "images/linguar/img1.jpg";
+	this.image2 = "images/linguar/img2.jpg";
+	this.image3 = "images/linguar/img3.jpg";
+	this.image4 = "images/linguar/img4.png";
+
+	this.video = "http://player.vimeo.com/video/98692480";
+}
+
+function floors(){
+	this.title = "Interactive Floors";
+
+	this.content = "A digital playground game for two teams of 3 players that uses the PlayStation Move Motion Controllers.<br/> "
+	this.content += "It was nominated at IndieCade 2014 as one of the 'Big Games' finalist."
+	this.mainContent = "The game consists of 3 rounds of 1 minute each. In which round both teams have to compete ";
+	this.mainContent += "to get the 'last egg'. When the game starts, the 'last egg' randomly selects one ";
+	this.mainContent += "PS Move and assigns a white light to it. This will indicate who was the egg."; 
+	this.mainContent += "<br/><br/>";
+	this.mainContent += "I was one of the programmers in this team."
+
+	this.image1 = "images/floors/img1.jpg";
+	this.image2 = "images/floors/img2.jpg";
+	this.image3 = "images/floors/img3.jpg";
+	this.image4 = "images/floors/img4.png";
+
+	this.video = "http://player.vimeo.com/video/98692480";
+}
+
+
